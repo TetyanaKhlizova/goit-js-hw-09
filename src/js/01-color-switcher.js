3ea379f389f4f91d8c1eee2989bb54da82e92741
TetyanaKhlizova <90279592+TetyanaKhlizova@users.noreply.github.com>
2022-02-13 10:49:20 +0200

    task01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -10,15 +10,23 @@
 
 const btnStart = document.querySelector('button[data-start]');
 const btnStop = document.querySelector('button[data-stop]');
-
+const body = document.querySelector('body');
 let timerId = null;
-
- btnStart.addEventListener('click', setInterval(getRandomHexColor(), 1000));
-
-btnStop.addEventListener("click", () => {
-  clearInterval(timerId);  
+btnStop.disabled = true;
+btnStart.addEventListener('click', () => {
+  timerId = setInterval(() => {    
+    body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+     btnStart.setAttribute('disabled', 'true');
+     btnStop.removeAttribute('disabled');
+      }, 1000);
 });
 
- function getRandomHexColor() {
-   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
- }
+btnStop.addEventListener("click", () => {
+  clearInterval(timerId); 
+  btnStart.removeAttribute('disabled');
+  btnStop.disabled = true;
+});
+
+
+
+ 
